cba44fe04fad570a53c005336473eb5f5919a590
Altimar <128001062+AltJuniior@users.noreply.github.com>
2024-11-14 22:06:42 +0000

    Questao3

---
+++ src/Questao3.java
@@ -0,0 +1,10 @@
+//Imprima todos os múltiplos de 3, entre 1 e 100
+
+public class Questao3 {
+    public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(3*i);
+
+        }
+    }
+}
