14c8573825752c684a148852ec9015b9bc0e53b7
Altimar <128001062+AltJuniior@users.noreply.github.com>
2024-11-14 22:02:59 +0000

    Questao2

---
+++ src/Questao2.java
@@ -0,0 +1,11 @@
+//Imprima a soma de 1 até 1000
+
+public class Questao2 {
+    public static void main(String[] args) throws Exception {
+        int soma=0;
+        for (int i = soma; i <= 1000; i++) {
+        soma += i;
+        }
+        System.out.println(soma);
+    }
+}
